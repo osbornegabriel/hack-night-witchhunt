e4fa24bcaffc29825d936edf4afd1c31bae0a6e0
Gabriel Osborne <osbornegabriel@gmail.com>
2017-10-11 19:49:40 -0500

    Build basic ajaxified start of game, where moderator selects number of players

--- app/controllers/game_controllers.rb
+++ app/controllers/game_controllers.rb
@@ -1,3 +1,15 @@
 # get '/games' do
 #  erb '/games'
 # end
+
+get '/games/new' do
+  if request.xhr?
+    erb :"games/new", layout: false
+  end
+end
+
+post '/games' do
+  p "88" * 88
+  p params
+  "yay"
+end
